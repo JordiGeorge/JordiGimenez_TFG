c2f6c7477bb0a1da65652c576880f30fa5bd15aa
JordiGeorge <jorgegim78@gmail.com>
2024-05-20 16:24:07 +0200

    MenuInventoryButton  fix
    InfoPanel and MenuInventoryButton merged

--- Assets/Scripts/MenuInventoryManager.cs
+++ Assets/Scripts/MenuInventoryManager.cs
@@ -4,8 +4,7 @@ public enum MenuIndex
 {
     Profile,
     Inventory,
-    Mission,
-    Log
+    Mission
 }
 
 public class MenuInventoryManager : MonoBehaviour
@@ -51,58 +50,72 @@ public class MenuInventoryManager : MonoBehaviour
         }
     }
     
-    public void InfoMenuPanel(int index)
+    public void MenuInventoryButton(int index)
     {
-        /*// Comprova si l'índex està fora del rang
-        if (index < 0 || index >= infoMenu.Length)
-        {
-            Debug.LogError("Índex fora de rang");
-            return;
-        }*/
-        
         SetState((MenuIndex)index);
 
-        switch (CurrentIndex)
-        {
-            case MenuIndex.Profile:
-                // Lògica per al primer GameObject a InfoMenu
-                infoMenu[0].SetActive(true);
-                infoMenu[1].SetActive(false);
-                infoMenu[2].SetActive(false);
-                break;
-            case MenuIndex.Inventory:
-                // Lògica per al segon GameObject
-                infoMenu[0].SetActive(false);
-                infoMenu[1].SetActive(true);
-                infoMenu[2].SetActive(false);
-                break;
-            case MenuIndex.Mission:
-                // Lògica per al tercer GameObject
-                infoMenu[0].SetActive(false);
-                infoMenu[1].SetActive(false);
-                infoMenu[2].SetActive(true);
-                break;
-            default:
-                Debug.LogError("Índex invàlid");
-                break;
-        }
-        
-    }
-    
-    // Mètode per entrar a GameState StoryMode
-    public void MenuInventoryButton()
-    {
         if (_gameStateManager.CurrentState != GameState.Inventory)
         {
             _uiManager.GameStateSwitcherUI(GameState.Inventory);
+            
             Time.timeScale = 0f;
+
+            switch (CurrentIndex)
+            {
+                case MenuIndex.Profile:
+                    // Lògica per al primer GameObject a InfoMenu
+                    infoMenu[0].SetActive(true);
+                    infoMenu[1].SetActive(false);
+                    infoMenu[2].SetActive(false);
+                    break;
+                case MenuIndex.Inventory:
+                    // Lògica per al segon GameObject
+                    _uiManager.GameStateSwitcherUI(GameState.Inventory);
+                    infoMenu[0].SetActive(false);
+                    infoMenu[1].SetActive(true);
+                    infoMenu[2].SetActive(false);
+                    break;
+                case MenuIndex.Mission:
+                    // Lògica per al tercer GameObject
+                    _uiManager.GameStateSwitcherUI(GameState.Inventory);
+                    infoMenu[0].SetActive(false);
+                    infoMenu[1].SetActive(false);
+                    infoMenu[2].SetActive(true);
+                    break;
+                default:
+                    Debug.LogError("Índex invàlid");
+                    break;
+            }
         }
         else if (_gameStateManager.CurrentState == GameState.Inventory)
         {
-            _uiManager.GameStateSwitcherUI(GameState.Navigation);
-            Time.timeScale = 1f;
+            if (index == 0)
+            {
+                infoMenu[0].SetActive(true);
+                infoMenu[1].SetActive(false);
+                infoMenu[2].SetActive(false);
+                _uiManager.GameStateSwitcherUI(GameState.Inventory);
+            }
+            else if (index == 1)
+            {
+                infoMenu[0].SetActive(false);
+                infoMenu[1].SetActive(true);
+                infoMenu[2].SetActive(false);
+                _uiManager.GameStateSwitcherUI(GameState.Inventory);
+            }
+            else if (index == 2)
+            {
+                infoMenu[0].SetActive(false);
+                infoMenu[1].SetActive(false);
+                infoMenu[2].SetActive(true);
+                _uiManager.GameStateSwitcherUI(GameState.Inventory);
+            }
+            else
+            {
+                _uiManager.GameStateSwitcherUI(GameState.Navigation);
+                Time.timeScale = 1f;
+            }
         }
-        else return;
     }
 }
 
